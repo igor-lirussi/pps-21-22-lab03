26c8ae9c03173f478630e1924c8ba5bdea15444e
igor-lirussi <igor.lirussi@studio.unibo.it>
2022-11-08 00:08:27 +0300

    es1 test and impl

--- src/main/scala/u03/Lists.scala
+++ src/main/scala/u03/Lists.scala
@@ -1,11 +1,14 @@
 package u03
 
-object Lists extends App:
+import scala.annotation.tailrec
+
+object Lists extends App :
 
   // A generic linkedlist
   enum List[E]:
     case Cons(head: E, tail: List[E])
     case Nil()
+
   // a companion object (i.e., module) for List
   object List:
 
@@ -21,6 +24,28 @@ object Lists extends App:
       case Cons(h, t) if pred(h) => Cons(h, filter(t)(pred))
       case Cons(_, t) => filter(t)(pred)
       case Nil() => Nil()
+
+    @tailrec
+    def drop[A](l: List[A], n: Int): List[A] = l match
+      case Cons(h, t) if n > 0 => drop(t, n - 1)
+      case Cons(h, t) if n == 0 => Cons(h, t)
+      case _ => Nil()
+
+    def append[A](left: List[A], right: List[A]): List[A] = left match
+      case Cons(h, t) => Cons(h, append(t, right))
+      case Nil() => right
+
+    def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] = l match
+      case Cons(h, t) => append(f(h), flatMap(t)(f))
+      case _ => Nil()
+
+    //mapper va da elemto a elemento, la flat map vuole da elemento a lista
+    //in ingresso funzione da  "x" a Lista( x mappato, nil) {case i => Cons(mapper(i), Nil()); case _ => Nil()}
+    def mapNew[A, B](l: List[A])(mapper: A => B): List[B] = flatMap(l)( x => Cons(mapper(x),Nil()) )
+
+    //passo alla flatmap una funzione che va da elemento generico a lista di elemento generico, ma solo se predicato vero, altrimenti va a lista vuota
+    //{case v if pred(v) => Cons(v, Nil()); case _ => Nil()}
+    def filterNew[A](l: List[A])(pred: A => Boolean): List[A] = flatMap(l)( x=>{if pred(x) then Cons(x, Nil()) else Nil()} )
 
   val l = List.Cons(10, List.Cons(20, List.Cons(30, List.Nil())))
   println(List.sum(l)) // 60
--- src/test/scala/u03/ListTest.scala
+++ src/test/scala/u03/ListTest.scala
@@ -5,6 +5,7 @@ import org.junit.Assert.*
 import Lists.*
 
 class ListTest:
+
   import List.*
 
   val l: List[Int] = Cons(10, Cons(20, Cons(30, Nil())))
@@ -20,3 +21,29 @@ class ListTest:
   @Test def testFilter() =
     assertEquals(Cons(20, Cons(30, Nil())), filter(l)(_ >= 20))
     assertEquals(Cons(10, Cons(30, Nil())), filter(l)(_ != 20))
+
+  @Test
+  def testDrop() =
+    assertEquals(Cons(20, Cons(30, Nil())), drop(l, 1))
+    assertEquals(Cons(30, Nil()), drop(l, 2))
+    assertEquals(Nil(), drop(l, 5))
+
+  @Test
+  def testAppend() =
+    val tail = Cons(40, Nil())
+    assertEquals(append(l, tail), Cons(10, Cons(20, Cons(30, Cons(40, Nil())))))
+    assertEquals(append(tail, l), Cons(40, Cons(10, Cons(20, Cons(30, Nil())))))
+    assertEquals(append(l, Nil()), l)
+
+  @Test
+  def testFlatMap() =
+    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), flatMap(l)(v => Cons(v + 1, Nil())))
+    assertEquals(Cons(11, Cons(12, Cons(21, Cons(22, Cons(31, Cons(32, Nil())))))), flatMap(l)(v => Cons(v + 1, Cons(v + 2, Nil()))))
+
+  @Test def testMapNew() =
+    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), mapNew(l)(_ + 1))
+    assertEquals(Cons("10", Cons("20", Cons("30", Nil()))), mapNew(l)(_ + ""))
+
+  @Test def testFilterNew() =
+    assertEquals(Cons(20, Cons(30, Nil())), filterNew(l)(_ >= 20))
+    assertEquals(Cons(10, Cons(30, Nil())), filterNew(l)(_ != 20))
